979e4af5c937118f9588379327b6b5dc7a5c903e
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-08 17:18:57 +0300

    form not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import "./app.scss"
+import Test from "./Test";
+import "./app.scss";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
-import Test from "./Test";
 
 const App = () => {
   return (
-  <div>
-    <section id="Homepage">
-      <Navbar/>
-      <Hero/>
-    </section>
-    <section id="Services">
-      <Parallax type="services"/>
-    </section>
-    <section>
-      <Services />
-    </section>
-    <section id="Portfolio">
-      <Parallax type="portfolio"/>
-    </section> 
-    <section>
+    <div>
+      {/* <Cursor /> */}
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <Portfolio />
-    </section>
-    <section id="Contact">Contact</section>
-
-  </div>
-  )
-  
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
 };
 
 export default App;
